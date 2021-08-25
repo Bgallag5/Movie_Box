157747748907246a3b715d7934e7b427596e2f19
Raevin Dunson <raevin.dunson98@gmail.com>
2021-08-25 01:51:59 -0400

    dashboard js updates to start click functions

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,14 @@
+// Call for buttons to activate carousel
+const prevButton = document.getElementById("prev-button")
+const nextButton = document.getElementById("next-button")
+
+function previous() {
+    prevButton.onclick
+}
+
+function next() {
+    nextButton.onclick
+}
+
+previous();
+next();
