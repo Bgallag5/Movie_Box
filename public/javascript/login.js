d4fcfe3ce76a13badeb8b80780e85ff87cf6931b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-08-25 21:10:58 -0400

    login html css updates and js calls

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,54 +1,6 @@
-async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-  
-  async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+// 
 
-  const loginButton= document.getElementById("login-button");
+  const loginButton = document.getElementById("login-button");
   var emailInput = document.getElementById("email-input");
   var passwordInput = document.getElementById("password-input");
   var usernameInput = document.getElementById("username-input");
@@ -58,6 +10,56 @@ async function loginFormHandler(event) {
     console.log(passwordInput.value);
     console.log(usernameInput.value);
   }
+
+  //async function loginFormHandler(event) {
+    //     event.preventDefault();
+      
+    //     const email = document.querySelector('#email-login').value.trim();
+    //     const password = document.querySelector('#password-login').value.trim();
+      
+    //     if (email && password) {
+    //       const response = await fetch('/api/users/login', {
+    //         method: 'post',
+    //         body: JSON.stringify({
+    //           email,
+    //           password
+    //         }),
+    //         headers: { 'Content-Type': 'application/json' }
+    //       });
+      
+    //       if (response.ok) {
+    //         document.location.replace('/dashboard/');
+    //       } else {
+    //         alert(response.statusText);
+    //       }
+    //     }
+    //   }
+      
+    //   async function signupFormHandler(event) {
+    //     event.preventDefault();
+      
+    //     const username = document.querySelector('#username-signup').value.trim();
+    //     const email = document.querySelector('#email-signup').value.trim();
+    //     const password = document.querySelector('#password-signup').value.trim();
+      
+    //     if (username && email && password) {
+    //       const response = await fetch('/api/users', {
+    //         method: 'post',
+    //         body: JSON.stringify({
+    //           username,
+    //           email,
+    //           password
+    //         }),
+    //         headers: { 'Content-Type': 'application/json' }
+    //       });
+      
+    //       if (response.ok) {
+    //         document.location.replace('/dashboard/');
+    //       } else {
+    //         alert(response.statusText);
+    //       }
+    //     }
+    //   }
 
   //const loginButton = document.getElementById("#login-button")
   //loginButton.onclick = function loginPage() {
