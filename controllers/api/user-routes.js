5157f6093300bc2d58267b5df016dc04ee7fe5ea
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-01 15:51:03 -0400

    updates and fixes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 
 
-const { User } = require("../../models");
+//const { User } = require("../../models");
 
 const { User , Movie} = require("../../models");
 
